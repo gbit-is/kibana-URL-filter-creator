7b298d5d5158dd7755ff835dbafdb6546aaabac8
Gbit <34913299+gbit-is@users.noreply.github.com>
2019-11-15 12:43:56 +0000

    bugfix
    extra pair of single quotes added around query parameters and the index ID added to the query string to fix a bug where the screen would partially blank out when attempts were made to update the filters

--- urlMaker.py
+++ urlMaker.py
@@ -16,12 +16,14 @@ config.sections()
 config.read('uuiDict.ini')
 
 
-port = 8098 # Port for flask server
+port = 8000 # Port for flask server
 
 
 
-base = "https://your_kibana_url/app/kibana#/dashboard/" # Base URL, check http/https in the url
-defaultBoardId = "75554d5f-6e8c-42a9-900c-958553423428" # If no ID is provided, this gets used (UUID provided is random)
+base = "https://your_kibana_url/app/kibana#/dashboard/" # Base URL
+defaultBoardId = "3f95-46b9-bf86-ae99dc03eabc" # If no ID is provided, this gets used
+defaultIndexId = "3f95-46b9-bf86-ae99dc03eabc" # required for proper functioning, otherwise dashboards break when you try to edit the filters
+
 time = "?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1h,to:now))" # Here the default time is set for dashboard view from this
 qhead = "&_a=(description:'',filters:!(" # Neccesary string to build the URL
 
@@ -48,31 +50,47 @@ def checkDict(name):
 
 
 # Here we create a single filter parameter 
-def makeParam(field,match): # field is the field and match is, what we wanna match, this only supports "field is something" filters 
-    base = '''('$state':(store:appState),meta:(disabled:!f,key:FIELD,negate:!f,params:(query:MATCH),type:phrase,value:MATCH),query:(match:(FIELD:(query:MATCH,type:phrase))))''' # Base string 
+def makeParam(field,match,index): # field is the field and match is, what we wanna match, this only supports "field is something" filters 
+    base = '''('$state':(store:appState),meta:(disabled:!f,index:'INDEX',key:FIELD,negate:!f,params:(query:MATCH),type:phrase,value:MATCH),query:(match:(FIELD:(query:MATCH,type:phrase))))''' # Base string 
     # populate the string with values
     base = base.replace("FIELD",field)
+    match = "'" + match + "'"
     base = base.replace("MATCH",match)
+    base = base.replace("INDEX",index)
     return base
 
 
 # creations of the URL
-def makeUrl(id,params): 
+def makeUrl(id,params,index): 
     
     # Params must be a nested list,for example: [ ["foo","123"],["bar":"asdf"] ]
     # If only one param is used it must be like this for example: [ ["foo","123"] ]
 
 
-    # check if a UUID was provided
+    # check if a UUID was provided for the dashboard
     if validate_uuid4(id):
         uuid = id
     else:
         # if not, check the dict
         uuid = checkDict(id)
 
-    # If neither a UUID nor an alias, return an error
+    # Check if the index refernece is a UUID
+    if validate_uuid4(index):
+        pass
+    else:
+        # If not, check the dict
+        indexName = index
+        index = checkDict(index)
+
+    
+    # If index is neither a UUID nor an alias, return an error
+    if index == "failed":
+        return "invalid?index=" + str(indexName) 
+
+
+    # If dashboard is neither a UUID nor an alias, return an error
     if uuid == "failed":
-        return "invalid?id=" + str(id)
+        return "invalid?id=" + str(id) 
 
     else:
 
@@ -84,7 +102,7 @@ def makeUrl(id,params):
         counter = 1
 
         for param in params:
-            res = makeParam(param[0],param[1]) # send the parameter provided to the makeParam function
+            res = makeParam(param[0],param[1],index) # send the parameter provided to the makeParam function
             url = url + res
         
             if counter == paramCount: # if we have reached the final parameter, close the brackets
@@ -107,9 +125,9 @@ app = flask.Flask(__name__)
 @app.route("/")
 def hello():
     args = request.args # collect args from url
-    id = 0 # Initalise ID for later comparison
+    id = defaultBoardId # Initalise ID as default, will be overwritten if argument is present
     params = [ ] # Initalise list for later nesting of params
-
+    index = defaultIndexId # Initialise index as default, will be overwritten if argument is present
     
     for arg in args: # For each argument provided
 
@@ -117,16 +135,15 @@ def hello():
         if arg == "id":
             id = args[arg]
         # All normal params get put into a list and then appended to the collection list
+        elif  arg == "_index":
+            index = args[arg]
         else:
             param = [ arg, args[arg] ]
             params.append(param)
 
-    # If no ID was provided, set the default ID as the ID
-    if id == 0:
-        id = defaultBoardId
 
 
-    url = makeUrl(id,params) # Send all params to the URL creation function
+    url = makeUrl(id,params,index) # Send all params to the URL creation function
     return redirect(url,code=302) # redirect the user to the generated URL
 
 # If an invalid UUID or alias is provided by the user, they get send here instead of a broken Kibana link
@@ -135,9 +152,10 @@ def hello():
 def invalid():
     args = request.args # Collect the args from the url
     for arg in args: # Only one parameter is created by the makeURL() function, so this method is not as terrible as it looks
-        id = args[arg]
+        type = str(arg)
+        value = str(args[arg])
     # Create a message for the user to recieve 
-    message = "<br><br><h2>You provided an invalid ID that is neither a UUID nor a defined alias<br>Id that was provided was:<b> " + str(id) + "</b></h2>"
+    message = "<br><br><h2>You provided an invalid " + type + " that is neither a UUID nor a defined alias<br>" + type + " that was provided was:<b> " + str(value) + "</b></h2>"
     return message
 
 
